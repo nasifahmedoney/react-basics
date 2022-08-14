839c35690d2c0cce97ecde4e8cc886121b0cdd88
nasifahmedoney <nasif.ahmed@live.com>
2022-08-14 23:16:00 +0600

    comments syntax and firebase

--- react-basics/src/pages/NewMeetup.js
+++ react-basics/src/pages/NewMeetup.js
@@ -4,6 +4,7 @@ function NewMeetupPage(){
 
     function addMeetUpHandler(meetupData){
         fetch(
+            // form data from NewMeetupForm is saved in firebase as table
             'https://react-getting-started-947f3-default-rtdb.firebaseio.com/meetups.json',
             {
                 method: 'POST',
@@ -19,6 +20,8 @@ function NewMeetupPage(){
         <section>
             <h1>new Meetup form</h1>
             <NewMeetupForm onAddMeetUp={addMeetUpHandler}/>
+            {/* check syntax and function definition
+             and parameter in NewMeetupForm props.onAddMeetUp(meetupFormData) and addMeetUpHandler(meetupData)*/}
         </section>
     );
 }
