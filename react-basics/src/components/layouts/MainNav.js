17d19928870433a8683589bd0401fbc6295078ec
nasifahmedoney <nasif.ahmed@live.com>
2022-08-20 19:46:35 +0600

    add remove items from favorites and favorites badge
    check-saving favorites data in browser storage and database

--- react-basics/src/components/layouts/MainNav.js
+++ react-basics/src/components/layouts/MainNav.js
@@ -1,22 +1,24 @@
 import { Link } from 'react-router-dom';
 import classes from './MainNav.module.css';
-
+import FavoritesContext from '../../store/favorites-context';
+import { useContext } from 'react';
 
 
 function MainNav(){
-    return (
+    const FavoriteCtx = useContext(FavoritesContext); 
+    return(
         <header className={classes.header}>
             <div className={classes.logo}>React meetups</div>
             <nav>
                 <ul>
                     <li>
-                        <Link to="/">All meetups page</Link>
+                        <Link to="/">All meetups</Link>
                     </li>
                     <li>
-                        <Link to="/favorite">Favorites page</Link>
+                        <Link to="/new-meetup">New meetups</Link>
                     </li>
                     <li>
-                        <Link to="/new-meetup">New meetups page</Link>
+                        <Link to="/favorite">Favorites<span className={classes.badge}>{FavoriteCtx.totalFavorites}</span></Link>
                     </li>
                 </ul>
             </nav>
--- react-basics/src/pages/Favorites.js
+++ react-basics/src/pages/Favorites.js
@@ -1,5 +1,23 @@
+import {useContext} from 'react';
+import FavoritesContext from '../store/favorites-context';
+import MeetupList from '../components/meetups/MeetupList';
+
 function FavoritesPage(){
-    return (<div>Favorites page</div>);
+
+    const favoritesCtx = useContext(FavoritesContext);
+
+    let items;
+
+    if(favoritesCtx.totalFavorites === 0){
+        items = <p>No favorite items</p>
+    }
+    else{
+        items = <MeetupList meetup={favoritesCtx.favorites} />
+    }
+    return(<section>
+        <h1>Favorites</h1>
+        {items}
+    </section>);
 }
 
 export default FavoritesPage;
